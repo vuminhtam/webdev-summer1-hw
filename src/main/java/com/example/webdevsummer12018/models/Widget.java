675169c4ee17a7119333b511a50155b263b9230b
Tam Vu <vu.t@husky.neu.edu>
2018-05-29 02:39:52 -0400

    add WidgetType

--- src/main/java/com/example/webdevsummer12018/models/Widget.java
+++ src/main/java/com/example/webdevsummer12018/models/Widget.java
@@ -1,5 +1,35 @@
 package com.example.webdevsummer12018.models;
 
-public class Widget {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Widget {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String text;
+	private String widgetType;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getWidgetType() {
+		return widgetType;
+	}
+	public void setWidgetType(String widgetType) {
+		this.widgetType = widgetType;
+	}
+	
 }
--- src/main/java/com/example/webdevsummer12018/services/WidgetService.java
+++ src/main/java/com/example/webdevsummer12018/services/WidgetService.java
@@ -3,13 +3,17 @@ package com.example.webdevsummer12018.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.webdevsummer12018.models.Widget;
 import com.example.webdevsummer12018.repositories.WidgetRepository;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class WidgetService {
 	@Autowired 
 	WidgetRepository widgetRepository;
@@ -17,5 +21,11 @@ public class WidgetService {
 	@GetMapping("/api/widget")
 	public List<Widget> findAllWidgets() {
 		return (List<Widget>) widgetRepository.findAll(); 
+	}
+	
+	@PostMapping("/api/widget/save")
+	public void saveAll(@RequestBody List<Widget> newList) {
+		widgetRepository.deleteAll();
+		widgetRepository.saveAll(newList);
 	}
 }
